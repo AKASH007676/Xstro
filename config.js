5d131eb4a1131fc3de73864cc793f098b128b96f
AKASH007676 <158271327+AKASH007676@users.noreply.github.com>
2024-12-17 00:53:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const config = {
-	SESSION_ID: process.env.SESSION_ID || 'XSTRO_92_41_03',
-	PREFIX: process.env.PREFIX || '.',
+	SESSION_ID: process.env.SESSION_ID || 'XSTRO_23_70_54',
+	PREFIX: process.env.PREFIX || '+',
 	SUDO: process.env.SUDO || '919064560840',
 	BOT_INFO: process.env.BOT_INFO || 'Astro;Xstro_Bot',
 	STICKER_PACK: process.env.STICKER_PACK || 'Xstro;Md',
 	WARN_COUNT: process.env.WARN_COUNT || 3,
-	CAPTION: 'ʙʏ xsᴛʀᴏ ᴍᴅ 𝟸𝟶𝟸𝟺',
+	CAPTION: 'ʙʏ akash ᴍᴅ 𝟸𝟶𝟸𝟺',
 	TIME_ZONE: process.env.TZ || process.env.TIME_ZONE || 'Africa/Lagos',
 	VERSION: require('./package.json').version,
 };
